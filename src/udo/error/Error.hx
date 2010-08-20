2a36313afa21ddaff543be758228e822a0259d83
Franco Ponticelli <franco.ponticelli@gmail.com>
2010-08-20 14:52:18 +0100

    improved error

--- src/udo/error/Error.hx
+++ src/udo/error/Error.hx
@@ -13,10 +13,9 @@ class Error extends Message
 {
 	public var pos(default,  null) : PosInfos;
 	public var inner(default, null) : Error;
-	public function new(message : String, ?params : Array<Dynamic>, ?param : Dynamic, ?inner : Error, ?pos : PosInfos)
+	public function new(message : String, ?params : Array<Dynamic>, ?param : Dynamic, ?pos : PosInfos)
 	{
 		super(message, params, param);
-		this.inner = inner;
 		this.pos = pos;
 	}
 	
@@ -27,7 +26,17 @@ class Error extends Message
 	}
 	
 	override public function toString()
-	{
-		return (null != inner ? inner.toString() + " " : "" ) + message.format(params);
+	{   
+		try {
+			return message.format(params);
+		} catch(e : Dynamic) {   
+			var ps = pos.className + "." + pos.methodName + "(" + pos.lineNumber + ")";  
+			var pa;
+			if(0 == params.length)              
+				pa = "no parameters passed";
+			else
+				pa = "wrong parameters passed ({0})";
+			throw new Error(pa + " for pattern '{1}' at {2}", [params.join(", "), message, ps]);
+		}
 	}
 }
